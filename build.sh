13432b2b2ab933ed4ad94051f686964d6814fbd4
JASKARAN SINGH <karan173740@gmail.com>
2025-07-12 01:07:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 echo "📦 Installing dependencies..."
 pip install -r requirements.txt
 
-# echo "🤖 Downloading Hugging Face model..."
-# python -c "from transformers import pipeline; pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')"
+echo "🤖 Downloading Hugging Face model..."
+python -c "from transformers import pipeline; pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')"
 
 echo "✅ Build completed successfully."
-# echo "✅ Hugging Face model downloaded and cached."
+echo "✅ Hugging Face model downloaded and cached."
